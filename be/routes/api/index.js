4281334819fef84fda40a06d6712d09f34960f6e
junginho <area1211@naver.com>
2018-11-25 19:09:28 +0900

    거리계산 기능 추가중

--- be/routes/api/index.js
+++ be/routes/api/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 
 router.use('/user', require('./user'));
 router.use('/dbtest', require('./dbtest'));
+router.use('/store', require('./store'));
 
 router.all('*', function(req, res, next) {
   next(createError(404, '존재하지 않음'));
