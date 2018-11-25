722f9aeca93cb08db15ccf0c28ecee96560ecbdf
Jisu Yuk <wltn3231@naver.com>
2018-11-25 19:44:29 +0900

    frontend-user 브랜치 생성

--- be/fe/src/views/user/mypage.vue
+++ be/fe/src/views/user/mypage.vue
@@ -24,8 +24,8 @@
       <!-- 내역 -->
       <v-container fluid grid-list-sm class="pa-0">
         <v-layout row wrap>
-          <v-flex v-for="i in 4" :key="i" xs3>
-            <img :src="`https://randomuser.me/api/portraits/men/${i + 20}.jpg`" class="image" alt="lorem" width="100%" height="100%">
+          <v-flex v-for="userMenu in userMenuList" :key="i" xs3>
+            <img :src="`${userMenu.url}`" class="image" alt="lorem" width="100%" height="100%" @click="goToPage(userMenu.page)">
           </v-flex>
         </v-layout>
       </v-container>
@@ -57,46 +57,21 @@
           </v-layout>
         </v-container>
       </v-flex>
-      <!-- 내역 -->
-      <v-container fluid grid-list-sm class="pa-0">
-        <v-layout row wrap>
-          <v-flex v-for="userMenu in userMenuList" :key="i" xs3>
-            <img :src="`${userMenu.url}`" class="image" alt="lorem" width="100%" height="100%">
-          </v-flex>
-        </v-layout>
-      </v-container>
+      
       <!-- 그 외 버튼 -->
       <v-flex xs12 sm12>
         <v-container grid-list-md text-xs-center align-center class="pa-0">
           <v-layout column wrap>
             
-            <v-flex xs12 sm12 class="py-3 grey--text text--lighten-5 orange lighten-3" @click="goToPage(noticePage)">
+            <v-flex 
+              v-for="anotherMenu in anotherMenuList" :key="i"
+              xs12 sm12 class="py-3 grey--text text--lighten-5 orange lighten-3" 
+              @click="goToPage(anotherMenu.page)">
               <div class="subheading font-weight-bold">
-                <span>공지사항</span>
+                <span>{{anotherMenu.name}}</span>
               </div>
             </v-flex>
-
             
-            <v-flex xs12 sm12 class="py-3 grey--text text--lighten-5 orange lighten-3" @click="goToPage(myInfoPage)">
-              <div class="subheading font-weight-bold">
-                <span>내정보 변경</span>
-              </div>
-            </v-flex>
-
-            
-            <v-flex xs12 sm12 class="py-3 grey--text text--lighten-5 orange lighten-3" @click="goToPage(qnaPage)">
-              <div class="subheading font-weight-bold">
-                <span>QnA</span> 
-              </div>
-            </v-flex>
-
-            
-            <v-flex xs12 sm12 class="py-3 grey--text text--lighten-5 orange lighten-3" @click="goToPage(settingPage)">
-              <div class="subheading font-weight-bold">
-                <span>환경 설정</span>
-              </div>
-            </v-flex>
-
           </v-layout>
         </v-container>
       </v-flex>
@@ -127,18 +102,44 @@ export default {
         userMenu1:{
           name: 'point-list',
           url: 'https://firebasestorage.googleapis.com/v0/b/angida-fe7f6.appspot.com/o/usermenu%2Fpoint.PNG?alt=media&token=c7d405ae-8998-46fd-8bb1-9a098d02de0a',
+          page:'http://localhost:8080/point',
         },
         userMenu2:{
           name: 'coupon-list',
           url: 'https://firebasestorage.googleapis.com/v0/b/angida-fe7f6.appspot.com/o/usermenu%2Fcoupon.PNG?alt=media&token=36e4eeac-f052-4123-bb64-bd89c7036d73',
+          page:'http://localhost:8080/coupon',
         },
         userMenu3:{
           name: 'order-list',
           url: 'https://firebasestorage.googleapis.com/v0/b/angida-fe7f6.appspot.com/o/usermenu%2Forder.PNG?alt=media&token=579ed091-4142-43a3-8ce3-6d15cc58ad9d',
+          page:'http://localhost:8080/reservationHistory',
         },
         userMenu4:{
           name: 'review-list',
           url: 'https://firebasestorage.googleapis.com/v0/b/angida-fe7f6.appspot.com/o/usermenu%2Freview.PNG?alt=media&token=9d7c78ab-1a29-4e66-a89a-92516c7b1a2f',
+          page:'http://localhost:8080/reviewManage',
+        },
+      },
+      anotherMenuList: {
+        anotherMenu1:{
+          name: '공지사항',
+          url: 'https://firebasestorage.googleapis.com/v0/b/angida-fe7f6.appspot.com/o/usermenu%2Fpoint.PNG?alt=media&token=c7d405ae-8998-46fd-8bb1-9a098d02de0a',
+          page:'http://localhost:8080/notice',
+        },
+        anotherMenu2:{
+          name: '내정보 변경',
+          url: 'https://firebasestorage.googleapis.com/v0/b/angida-fe7f6.appspot.com/o/usermenu%2Fcoupon.PNG?alt=media&token=36e4eeac-f052-4123-bb64-bd89c7036d73',
+          page:'http://localhost:8080/myInfoChange',
+        },
+        anotherMenu3:{
+          name: 'QnA',
+          url: 'https://firebasestorage.googleapis.com/v0/b/angida-fe7f6.appspot.com/o/usermenu%2Forder.PNG?alt=media&token=579ed091-4142-43a3-8ce3-6d15cc58ad9d',
+          page:'http://localhost:8080/Q&A',
+        },
+        anotherMenu4:{
+          name: '환경설정',
+          url: 'https://firebasestorage.googleapis.com/v0/b/angida-fe7f6.appspot.com/o/usermenu%2Freview.PNG?alt=media&token=9d7c78ab-1a29-4e66-a89a-92516c7b1a2f',
+          page:'http://localhost:8080/setting',
         },
       }
     }
