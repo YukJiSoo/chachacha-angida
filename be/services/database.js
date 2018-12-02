c866768ebd5c8a3172758b0610da1d157178b757
junginho <area1211@naver.com>
2018-12-02 16:23:04 +0900

    transaction ...ing

--- be/services/database.js
+++ be/services/database.js
@@ -44,7 +44,7 @@ function simpleExecute(statement, binds = [], opts = {}) {
 
 module.exports.simpleExecute = simpleExecute;
 
-
+const firstQuery = `insert into RESERV_ORDER ( store_code, customer_code, order_time, reserv_time, no_of_people, total_price, point_discount, coupon_discount, order_status, review_status ) values ( :store_code, :customer_code, :order_time, :reserv_time, :no_of_people, :total_price, :point_discount, :coupon_discount, :order_status, :review_status )`
 function simpleTrasaction (binds) {
   return new Promise(async (resolve, reject) => {
     let conn;
@@ -56,9 +56,7 @@ function simpleTrasaction (binds) {
       conn.transaction([
         // first : RESERV_ORDER 테이블에 예약 정보 INSERT
         function firstAction(binds, callback) {
-          conn.insert(
-            `insert into RESERV_ORDER ( store_code, customer_code, order_time, reserv_time, no_of_people, total_price, point_discount, coupon_discount, order_status, review_status ) values ( :store_code, :customer_code, :order_time, :reserv_time, :no_of_people, :total_price, :point_discount, :coupon_discount, :order_status, :review_status )`
-            binds,
+          conn.insert(firstQuery, binds,
             function onResults(error, output) {
               //continue flow...
             });
@@ -66,7 +64,7 @@ function simpleTrasaction (binds) {
         // second : 예약된 메뉴 개수 만큼, MENU_ORDER_ITEM 테이블에 예약된 메뉴 정보 INSERT
         function secondAction(binds, callback) {
           conn.insert(
-            `insert into MENU_ORDER_ITEM ( order_code, store_code, menu_code ) values ( :order_code, :store_code, :menu_code )`
+            `insert into MENU_ORDER_ITEM ( order_code, store_code, menu_code ) values ( :order_code, :store_code, :menu_code )`,
             binds,
             function onResults(error, output) {
               //continue flow...
@@ -75,7 +73,7 @@ function simpleTrasaction (binds) {
         // third : 쿠폰을 사용한 경우, CUSTOMER_COUPON 테이블에 쿠폰 상태를 '사용했다'고 UPDATE
         function thirdAction(binds, callback) {
           conn.insert(
-            `update CUSTOMER_COUPON set where`
+            `update CUSTOMER_COUPON set where`,
             binds,
             function onResults(error, output) {
               //continue flow...
@@ -84,7 +82,7 @@ function simpleTrasaction (binds) {
         // forth : 포인트 사용한 경우, CUSTOMER_POINT 테이블에 사용한 포인트(point_discount)만큼 차감 UPDATE
         function forthAction(binds, callback) {
           conn.insert(
-            `update CUSTOMER_POINT set where`
+            `update CUSTOMER_POINT set where`,
             binds,
             function onResults(error, output) {
               //continue flow...
@@ -93,7 +91,7 @@ function simpleTrasaction (binds) {
         // fifth : PAYMENT 테이블에 예약된 결제 정보 INSERT
         function fifthAction(binds, callback) {
           conn.insert(
-            `insert into PAYMENT ( order_code, payment_date, store_name, payment_amount, payment_status ) values ( :order_code, SYSDATE, :store_name, :payment_amount, :payment_status )`
+            `insert into PAYMENT ( order_code, payment_date, store_name, payment_amount, payment_status ) values ( :order_code, SYSDATE, :store_name, :payment_amount, :payment_status )`,
             binds,
             function onResults(error, output) {
               //continue flow...
